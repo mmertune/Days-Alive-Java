84118836912a01301772fc7f34deb3eebd56b021
Marvin <61062647+mmertune@users.noreply.github.com>
2021-02-09 20:49:16 -0500

    Added Comments and fixed structure of the file

--- DaysAlive.java
+++ DaysAlive.java
@@ -1,18 +1,20 @@
-
-
 public class DaysAlive {
     public static void main(String[] args) {
 
+        //initializes integers for year, month and day for the date of birth for user
         int dobYear = 1999;
         int dobMonth = 6;
         int dobDay = 15;
         
+        //Constructs two methods from Day class for the date of birth and today's date
         Day dateOfBirth = new Day(dobYear, dobMonth, dobDay);
         Day todayDate = new Day();
-
+        
+        //calculates the days from dateOfBirth to todayDate
         int daysSinceBirth = todayDate.daysFrom(dateOfBirth);
        
-        System.out.println("Number of Days from birth: ");
-        System.out.print(daysSinceBirth);
+        //Prints out the number of days user has been alive
+        System.out.print("Number of days alive since birth: ");
+        System.out.println(daysSinceBirth);
     }
 }
